f7eba9268095208d503dbaa5a34bdd03ee908b25
Marius Schueller <mariuseschueller@gmail.com>
2024-07-09 21:42:23 -0500

    alpha beta prunning

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ class Solver:
     def __init__(self):
         self.node_count = 0
 
-    def negamax(self, p):
+    def negamax(self, p, alpha, beta):
         self.node_count += 1  # Increment counter of explored nodes
 
         if p.nb_moves() == p.WIDTH * p.HEIGHT:  # Check for draw game
@@ -15,7 +15,12 @@ class Solver:
             if p.can_play(x) and p.is_winning_move(x):
                 return (p.WIDTH * p.HEIGHT + 1 - p.nb_moves()) // 2
 
-        best_score = -p.WIDTH * p.HEIGHT  # Init the best possible score with a lower bound of score.
+        max = (p.WIDTH * p.HEIGHT + 1 - p.nb_moves()) // 2  # Init the best possible score with a lower bound of score.
+
+        if beta > max:
+            beta = max
+            if alpha >= beta:
+                return beta
 
         for x in range(p.WIDTH):
             if p.can_play(x):
@@ -23,17 +28,23 @@ class Solver:
                 p2.board = [row[:] for row in p.board]
                 p2.height = p.height[:]
                 p2.moves = p.moves
+
                 p2.play(x)
-                score = -self.negamax(p2)
-                if score > best_score:
-                    best_score = score
 
-        return best_score
+                score = -self.negamax(p2, -beta, -alpha)
 
-    def solve(self, p, weak=False):
+                if score >= beta:
+                    return score
+                if score > alpha:
+                    alpha = score
+        return alpha
+
+    def solve(self, p, weak = False):
         self.node_count = 0
-
-        return self.negamax(p)
+        if weak:
+            return self.negamax(p, -1, 1)
+        else:
+            return self.negamax(p, -position.Position.WIDTH * position.Position.HEIGHT / 2, position.Position.WIDTH * position.Position.HEIGHT / 2)
 
     def get_node_count(self):
         return self.node_count
